2e3db3b14d2e29bc00b5d0b921bf9799b85d26fd
iAmShivay <shivaysharma77894@gmail.com>
2025-01-09 12:49:33 +0530

    update database connection; change MongoDB URI to use Atlas and improve code formatting

--- backend/project/src/config/db.js
+++ backend/project/src/config/db.js
@@ -1,14 +1,16 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/content_platform');
-    console.log('MongoDB connected successfully');
+    await mongoose.connect(
+      "mongodb+srv://shivaysharma77893:Shivkumar777%40@cluster0.ofqzc.mongodb.net/content_platform"
+    );
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   }
 };
